1a84926d02978cc4a33ae01ccdb2841ec758c06f
arthur-cabral <arthurmartins10052002@gmail.com>
2023-11-14 03:32:51 -0300

    build: Fixing config files to build correctly

--- fe-digital-circle/src/main.ts
+++ fe-digital-circle/src/main.ts
@@ -1,7 +1,19 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(HttpClientModule),
+    provideAnimations()
+]
+});
