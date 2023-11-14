3275469b99f4b207adbef71ef7c5c14a8cec000e
arthur-cabral <arthurmartins10052002@gmail.com>
2023-11-14 16:22:21 -0300

    chore: Implementing CRUD and fixing pagination

--- fe-digital-circle/src/app/modules/tb01/tb01-crud/tb01-crud.component.ts
+++ fe-digital-circle/src/app/modules/tb01/tb01-crud/tb01-crud.component.ts
@@ -1,11 +1,15 @@
 import { Component, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { Tb01 } from 'src/app/models/tb01';
+import { Tb01 } from 'src/app/models/tb01/tb01';
 import { Tb01Service } from 'src/app/services/tb01.service';
 import { Tb01CreateFormComponent } from '../tb01-create-form/tb01-create-form.component';
 import { MatTableDataSource } from '@angular/material/table';
-import { MatPaginator } from '@angular/material/paginator';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
+import { PaginationParameters } from 'src/app/models/pagination/PaginationParameters';
+import { NgxSpinnerService } from 'ngx-spinner';
+import Swal from 'sweetalert2';
+import { Tb01UpdateFormComponent } from '../tb01-update-form/tb01-update-form.component';
 
 @Component({
   selector: 'app-tb01-crud',
@@ -14,8 +18,16 @@ import { MatSort } from '@angular/material/sort';
 })
 export class Tb01CrudComponent {
 
-  dataSource: MatTableDataSource<Tb01>;
   displayedColumns: string[] = ['id', 'colTexto', 'colDt', 'actions'];
+
+  public length = 0;
+  public pageSize = 10;
+  public pageIndex = 0;
+  public pageSizeOptions: number[] = [10, 15, 20, 30];
+  public dataSource!: any;
+  public pageSlice!: any;
+
+  public tb01: Tb01[] = [];
 
   @ViewChild(MatPaginator)
   paginator!: MatPaginator;
@@ -25,52 +37,108 @@ export class Tb01CrudComponent {
 
   constructor(
     private tb01Service: Tb01Service,
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private spinner: NgxSpinnerService,
   ) {
-    this.dataSource = new MatTableDataSource<Tb01>();
+    this.dataSource = new MatTableDataSource<Tb01>([]);
   }
 
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+  ngOnInit() {
+    this.getData();
   }
 
-  async ngOnInit() {
-    this.getAllTb01();
+  async getData() {
+    await this.count();
+    await this.getAllTb01();
+  }
+
+  async count() {
+    await this.tb01Service.count().subscribe(data => {
+      this.length = 0;
+      this.length = data;
+    });
   }
 
   async getAllTb01() {
-    this.tb01Service.getAllTb01().subscribe(
-      data => {
-        this.dataSource.data = data.map(element => ({
-          id: element.id,
-          colTexto: element.colTexto,
-          colDt: element.colDt
-        }));
-      },
-      error => {
-        console.error('Erro ao obter dados do serviço:', error);
+    var paginationParameters: PaginationParameters = {
+      PageSize: this.pageSize,
+      PageNumber: this.pageIndex
+    };
+
+    console.log(paginationParameters)
+    this.spinner.show();
+    await this.tb01Service.getAllTb01(paginationParameters).subscribe(data => {
+      if (data.length == 0) {
+        this.tb01 = [];
       }
-    );
+      this.tb01 = [];
+      this.tb01 = data;
+      this.pageSlice = this.tb01.slice(0, this.pageSize);
+      this.dataSource = new MatTableDataSource(this.pageSlice);
+
+      this.spinner.hide();
+    });
   }
 
-  editItem(element: Tb01) {
-    console.log(element);
-  }
-
-  deleteItem(element: Tb01) {
-    console.log(element);
+  paginate(event: PageEvent) {
+    console.log(event)
+    this.pageSize = event.pageSize;
+    this.pageIndex = event.pageIndex;
+    this.getAllTb01();
   }
 
   openCreateForm() {
-    const dialogRef = this.dialog.open(Tb01CreateFormComponent, {
+    this.dialog.open(Tb01CreateFormComponent, {
       width: '400px',
       data: {}
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('O formulário foi fechado com o resultado:', result);
+    }).afterClosed().subscribe(result => {
+      this.getAllTb01();
     });
   }
 
+  openUpdateForm(element: any) {
+    this.dialog.open(Tb01UpdateFormComponent, {
+      width: '400px',
+      data: {
+        id: element.id,
+        colTexto: element.colTexto,
+      }
+    }).afterClosed().subscribe(result => {
+      this.getAllTb01();
+    });
+  }
+
+  openDeleteModal(element: any) {
+    Swal.fire({
+      title: "Tem certeza?",
+      text: "Você não poderá reverter essa ação!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Delete"
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        this.deleteElement(element);
+      }
+    });
+  }
+
+  async deleteElement(element: any) {
+    var tb01: Tb01 = {
+      id: element.id,
+    }
+    await this.tb01Service.deleteTb01(tb01).subscribe(() => {
+      this.getAllTb01();
+    })
+    Swal.fire({
+      title: "Deletado!",
+      text: "Seu registro foi deletado.",
+      icon: "success"
+    });
+  }
+
+
+
 }
+
--- fe-digital-circle/src/app/services/tb01.service.ts
+++ fe-digital-circle/src/app/services/tb01.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Tb01 } from '../models/tb01';
+import { Tb01 } from '../models/tb01/tb01';
 import { Observable } from 'rxjs';
+import { PaginationParameters } from '../models/pagination/PaginationParameters';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,23 @@ export class Tb01Service {
     private httpClient: HttpClient
   ) { }
 
-  getAllTb01(): Observable<Tb01[]> {
-    return this.httpClient.get<Tb01[]>(this.url);
+  getAllTb01(paginationParameters: PaginationParameters): Observable<Tb01[]> {
+    return this.httpClient.get<Tb01[]>(this.url + '?PageNumber=' + paginationParameters.PageNumber + '&PageSize=' + paginationParameters.PageSize);
+  }
+
+  count(): Observable<number> {
+    return this.httpClient.get<number>(this.url + '/count');
+  }
+
+  createTb01(tb01: Tb01): Observable<Tb01> {
+    return this.httpClient.post<Tb01>(this.url + '/create', tb01);
+  }
+
+  updateTb01(tb01: Tb01): Observable<Tb01> {
+    return this.httpClient.post<Tb01>(this.url + '/update', tb01);
+  }
+
+  deleteTb01(tb01: Tb01): Observable<Tb01> {
+    return this.httpClient.post<Tb01>(this.url + '/delete', tb01);
   }
 }
