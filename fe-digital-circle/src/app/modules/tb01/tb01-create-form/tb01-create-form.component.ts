a38540aa17ea328c9a28bd0b4dbfd582829f2701
arthur-cabral <arthurmartins10052002@gmail.com>
2023-11-14 16:20:24 -0300

    chore: Building tb01-create-form component

--- fe-digital-circle/src/app/modules/tb01/tb01-create-form/tb01-create-form.component.ts
+++ fe-digital-circle/src/app/modules/tb01/tb01-create-form/tb01-create-form.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { Tb01Service } from 'src/app/services/tb01.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-tb01-create-form',
@@ -7,4 +12,44 @@ import { Component } from '@angular/core';
 })
 export class Tb01CreateFormComponent {
 
+  form: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private tb01Service: Tb01Service,
+    protected $dialog: MatDialog,
+    private spinner: NgxSpinnerService,
+  ) {
+    this.form = this.formBuilder.group({
+      colTexto: new FormControl('', []),
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  async onSubmit() {
+    this.spinner.show();
+    await this.tb01Service.createTb01(this.form.value).subscribe(data => {
+      if (data) {
+        this.spinner.hide();
+        this.$dialog.closeAll();
+        Swal.fire({
+          icon: 'success',
+          title: 'Sucesso',
+          text: 'Registro inserido com sucesso',
+          showConfirmButton: false,
+          timer: 1500
+        })
+      } else {
+        Swal.fire({
+          icon: 'error',
+          title: 'Erro',
+          text: 'Erro ao inserir',
+          showConfirmButton: true,
+        })
+        this.spinner.hide();
+      }
+    })
+  }
 }
